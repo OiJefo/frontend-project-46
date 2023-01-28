dece16f68b2c84ee45272d9b5da27044f8a55827
Mike Po <oijefo@gmail.com>
2023-01-29 01:18:27 +0300

    badges, lint, tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,6 +1,19 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { test, expect } from '@jest/globals';
 import genDiff from '../index';
 
-test('genDiff', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{\n  + follow: false\n  host: hexlet.io\n  + proxy: 123.234.53.22\n  - timeout: 20\n  + timeout: 50\n  - verbose: true\n}');
+const filepath1 = '__fixtures__/file1.json';
+const filepath2 = '__fixtures__/file2.json';
+const expected = '{\n  + follow: false\n  host: hexlet.io\n  + proxy: 123.234.53.22\n  - timeout: 20\n  + timeout: 50\n  - verbose: true\n}';
+const cases = [[filepath1, filepath2, expected]];
+
+// eslint-disable-next-line no-undef
+describe('genDiff example', () => {
+  test.each(cases)(
+    'given %p and %p as arguments, returns %p',
+    (firstArg, secondArg, expectedResult) => {
+      const result = genDiff(firstArg, secondArg);
+      expect(result).toEqual(expectedResult);
+    },
+  );
 });
