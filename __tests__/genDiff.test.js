4c1be3c607bc32ddb3ab49f20f6b430d14a9bae9
Mike Po <oijefo@gmail.com>
2023-01-29 00:34:43 +0300

    badges, lint, tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,5 +2,5 @@ import { test, expect } from '@jest/globals';
 import genDiff from '../index';
 
 test('genDiff', () => {
-  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toBe(3);
+  expect(genDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual('{\n  + follow: false\n  host: hexlet.io\n  + proxy: 123.234.53.22\n  - timeout: 20\n  + timeout: 50\n  - verbose: true\n}');
 });
