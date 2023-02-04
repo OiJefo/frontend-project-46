4fe38a7cb7e0ada54a4753a013272f729d4ddb2a
Mike Po <oijefo@gmail.com>
2023-02-05 00:58:14 +0300

    mission acomplished

--- src/parser.js
+++ src/parser.js
@@ -2,12 +2,12 @@ import yaml from 'js-yaml';
 
 const parse  = (file, type) => {
   switch (type) {
-    case 'yml':
-    case 'yaml':
-      return yaml.load(file);
     case 'json':
     default:
       return JSON.parse(file);
+    case 'yml':
+    case 'yaml':
+      return yaml.load(file);
   }
 };
 
